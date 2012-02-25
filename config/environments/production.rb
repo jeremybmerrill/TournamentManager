e56a4df0120cd1ac8722b7f21249e73796515077
Jeremy B. Merrill <jeremy@jeremybmerrill.com>
2012-02-25 11:34:06 -0800

    added host for actionmailer to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,7 @@ TournamentManager::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'tabroom.herokuapp.com' }
 
   # Enable threaded mode
   # config.threadsafe!

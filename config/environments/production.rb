9115af0a580ce81cf228661a0db5ed180d9e2b75
Jeremy B. Merrill <jeremy@jeremybmerrill.com>
2012-02-25 19:00:14 -0800

    more email stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,17 @@ TournamentManager::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'tabroom.herokuapp.com' }
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.sendgrid.net",
+  :port => '25',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+
+}
+
+
 
   # Enable threaded mode
   # config.threadsafe!
--- config/setup_mail.rb
+++
@@ -1,8 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address => "smtp.sendgrid.net",
-  :port => '25',
-  :domain => "tabroom.info",
-  :authentication => :plain,
-  :user_name => "app2778424@heroku.com",
-  :password => ""
-}

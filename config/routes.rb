6dc62410ebeeab58c0ec9097aa9c79f7348654ad
Jacinth Sohi <jacinthsohi@gmail.com>
2012-02-25 18:26:47 -0800

    actually fixed route to home

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ TournamentManager::Application.routes.draw do
 end
 
 
-  root :to => "/"
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

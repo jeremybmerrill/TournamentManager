860733ecc13993d55ffce6be4f219c256a3ee04b
Jeremy B. Merrill <jeremy@jeremybmerrill.com>
2012-02-25 18:09:58 -0800

    fixing on-the-fly ballot running total script

--- public/javascripts/amtaballot.js
+++ public/javascripts/amtaballot.js
@@ -2,18 +2,23 @@ jQuery(document).ready(function(){
   /* Do running totals */
 
   jQuery('input.d.score').change(function(){
-    var dtotal;
+    var ptotal;
     ptotal = 0;
     jQuery('input.d.score').each(function(){
       try{
         var parsed;
         parsed = parseInt(jQuery(this).val());
-        if (!isNaN(parsed)){
-          dtotal += parsed;
+        if (!isNaN(parsed)){ /* add number check here */
+          ptotal += parsed;
+          jQuery(this).removeClass('failbox');
+        }else{
+          if(jQuery(this).val() != ""){
+            jQuery(this).addClass('failbox');
+          }
         }
       }catch(e){}
     });
-    jQuery('span#d_total').text(dtotal);
+    jQuery('span#d_total').text(ptotal);
   });
 
   jQuery('input.p.score').change(function(){
